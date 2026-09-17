fd009fbec8ec3b87083a0f3b1d49aebcec5397f6
gourmet0828 <yhpark0828@naver.com>
2026-09-17 22:02:06 +0900

    gourmet fault

--- src/main/java/com/audigo/domain/auth/dto/AuthTokens.java
+++ src/main/java/com/audigo/domain/auth/dto/AuthTokens.java
@@ -3,7 +3,6 @@ package com.audigo.domain.auth.dto;
 public record AuthTokens(
         String accessToken,
         String refreshToken,
-        String tokenType,
         long accessTokenExpiresIn,
         long refreshTokenExpiresIn
 ) {
